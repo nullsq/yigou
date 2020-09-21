8c467aacf4bb0983055964ce664c504dc1eef8aa
a212167554 <1072671677@qq.com>
2020-09-21 11:16:46 +0800

    可以了

---
+++ client/src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <div id="app">
+	<div>
+		<!-- <button>home</button> -->
+		<!-- <button>about</button> -->
+		<!-- <a href="http://192.168.3.113:8080/">home</a> &nbsp; 
+		<a href="http://192.168.3.113:8080/about">about</a> -->
+		
+		<!-- <a href="/">home</a> &nbsp;
+		<a href="/about">about</a> -->
+		
+		<router-link to="/">home</router-link>&nbsp; 
+		<router-link to="/about">about</router-link>
+	</div>
+  
+    <router-view/>
+  </div>
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
---
+++ client/src/router/index.js
@@ -0,0 +1,25 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Home from '../views/Home.vue'
+
+
+
+Vue.use(VueRouter)
+
+const router = new VueRouter({
+  mode: 'history',
+  routes:[
+  {
+    path: '/',
+    name: 'Home',
+    component: Home
+  },
+  {
+    path: '/about',
+    name: 'About',
+    component: () => import('../views/About.vue')
+  }
+]
+})
+
+export default router
